9d3ec01abfb2a91baa140aaa2c0cb599abb5ca83
Ali Jaan <129405358+Ali-Jaan-Butt@users.noreply.github.com>
2024-03-02 20:05:01 +0500

    main

---
+++ .ipynb_checkpoints/mydb-checkpoint.py
@@ -0,0 +1,8 @@
+import requests
+import json
+class Mydb():
+    def sign_up(self, name, email, password):
+        signup_url = 'https//:www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key='
+        pass
+    def sign_in(self):
+        pass
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.uix.screenmanager import Screen
 from kivymd.uix.card import MDCard
 from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
+from mydb import Mydb
+import requests
+import json
 
 
 class StartScreen(Screen):
@@ -27,7 +30,14 @@ GUI = Builder.load_file('main.kv')
 
 class MainApp(App):
     def build(self):
+        self.my_db = Mydb()
         return GUI
+    
+    def on_start(self):
+        res = requests.get('https://med-care-35b0f-default-rtdb.firebaseio.com/1.json')
+        print('Is it ok???', res.ok)
+        data = json.loads(res.content.decode())
+        print(data)
 
     def change_screen(self, screen):
         screen_manager = self.root.ids['screen_manager']
